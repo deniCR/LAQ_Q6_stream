d5ef5a845f65174294cdca1f93bec5fc48761df1
Daniel <31859571+deniCR@users.noreply.github.com>
2019-01-30 23:58:37 +0000

    Update consumer.hpp

--- include/consumer.hpp
+++ include/consumer.hpp
@@ -19,22 +19,16 @@ namespace stream {
 	template<typename T>
 	class Consumer {
 		typedef Data_Stream_struct<T> Data_stream;
-		//canal de comunicação entre o produtor e o consumidor
 		typedef lockfree::queue<Data_stream*> Channel;
 
 	private:
-		//Número máximo de threads ativas
 		int max_threads=1;
 
 	public:
-		//Canal de comunicação 
 		Channel *in = new Channel(1024);
-		//Variável de sinalização
 		boost::atomic<bool> *consumer_done = new boost::atomic<bool>(false);
-		//Tempo de suspensão (ns)
 		long wait=1500;
 
-		//Construtor vazio
 		Consumer() {}
 		Consumer(int _max_thread):max_threads(_max_thread)
 		{}
@@ -47,14 +41,8 @@ namespace stream {
 			*/
 		}
 
-		//A operação deve descrever a ação a tomar para o processamento de 
-		// um único elemento da stream **** Ainda não implementado !!!!!
-		//Operação do Consumidor
 		virtual void operation() {}
 
-		//Retorna o proximo elemento
-		//Se o produtor já acabou de produzir os elementos o pop pode retornar NULL
-		//quando o pop retorna false já não há mais elementos para processar.
 		bool pop_next2(Data_stream** next){
 			bool result = false;
 			if(!(*consumer_done) || !(in->empty())){
@@ -66,7 +54,7 @@ namespace stream {
 			}
 			return result;
 		}
-		//Mecanismos de lock(suspensão da thread quando está a espera de novos dados)
+
 		bool pop_next(Data_stream** next){
 			bool result = false;
 			int i=0;
@@ -85,7 +73,7 @@ namespace stream {
 				wait=(wait+(i*1000))/2;
 			return result;
 		}
-		//Execução da operação com multithread
+
 		virtual void run()
 		{
 			std::vector<boost::thread*> threads;
@@ -100,7 +88,6 @@ namespace stream {
 			}
 		}
 
-		//Execução da operação com apenas uma thread
 		virtual void run_seq()
 		{
 			auto consumer_thread = new thread(&Consumer::operation, this);
@@ -108,7 +95,6 @@ namespace stream {
 			consumer_thread->join();
 		}
 
-		//Execução sequencial, sem controlo de elementos
 		virtual void run_one_time(){
 			operation();
 		}
