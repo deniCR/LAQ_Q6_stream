70c6a56a4855c297a663ff4a00b58941fb9b4f36
Daniel <31859571+deniCR@users.noreply.github.com>
2019-01-31 00:00:07 +0000

    Update join.hpp

--- include/join.hpp
+++ include/join.hpp
@@ -19,12 +19,6 @@ using namespace boost;
 
 namespace stream {
 
-	/* 
-	 * A classe Join pretende agregar dois inputs para um output
-	 * Ex: input1: <id_1,Block_1> input2: <id_2,Block_2> 
-	 * 			==(id_1=id_2=id)=> output: <id,<Block_1,Block_2>>
-	 * Os inputs são organizados pelos ids, mantendo um certa "ordem".
-	 */
 	template<typename P1, typename P2>
 	class Join: 
 		public Producer<std::tuple<P1,P2>*>,
@@ -32,27 +26,23 @@ namespace stream {
 	{
 		typedef Data_Stream_struct<P1> Elem1;
 		typedef Data_Stream_struct<P2> Elem2;
-		typedef std::tuple<P1,P2> Join_data;			//Elementos do Input1 e Input2
+		typedef std::tuple<P1,P2> Join_data;
 		typedef Data_Stream_struct<Join_data*> Data_stream;
-		typedef lockfree::queue<Elem2*> Channel;		//channel adicional
+		typedef lockfree::queue<Elem2*> Channel;
 	
 	private:
-  		unordered_map<ID,P1> map1;				//Map dos Inputs de 1
-  		unordered_map<ID,P2> map2;				//Map dos Inputs de 2
+  		unordered_map<ID,P1> map1;
+  		unordered_map<ID,P2> map2;
 
 	public:
-		//channel adicional
 		Channel *in_join = new Channel(1024);
-		//Variável de sinalização adicional
 		boost::atomic<bool> *done_join = new boost::atomic<bool>(false);
 
-		//Construtor
 		Join(int _max_thread, Consumer<Join_data*> next):
 		Producer<Join_data*>(next), Consumer<P1>(_max_thread){}
 
 		virtual ~Join(){}
 
-		//Search and send
 		void search_map1(Elem2 *value){
 			ID id = 0;
 			if(value!=NULL){
@@ -69,7 +59,7 @@ namespace stream {
 		  	}
 		  	value=NULL;
 		}
-		//Search and send
+
 		void search_map2(Elem1 *value){
 			ID id = 0;
 			if(value!=NULL){
@@ -91,7 +81,6 @@ namespace stream {
 		using Producer<std::tuple<P1,P2>*>::run;
 		using Producer<std::tuple<P1,P2>*>::run_seq;
 
-		//Função de join
 		inline void operation(void) override
 		{
 			Elem1 *value1=NULL;
