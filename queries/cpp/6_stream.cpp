7a4063bdcacad51231de0c7f6b69469dd9b267c0
denicr <a75655@alunos.uminho.pt>
2019-02-11 17:15:35 +0000

    Add query

--- queries/cpp/6_stream.cpp
+++ queries/cpp/6_stream.cpp
@@ -9,7 +9,7 @@
 #include "include/StreamComponents.hpp"
 
 #ifndef DATASET
-  #define DATASET "4"
+  #define DATASET "8"
 #endif
 
 using namespace engine;
@@ -57,87 +57,60 @@ int main() {
 
   auto *filter_a = new Filter_LabelBlock(var_a_pred,1);
   auto *loader_a1 = new Load_LabelBlock(var_lineitem_shipdate, *filter_a);
-  Sum *sum = new Sum(var_h,1);
-  auto *hadamard_s = new Hadamard_DecimalVectorBlock(var_g,1,*sum);
-  auto *join_s  = new Join< FilteredBitVectorBlock*,
-                            DecimalVectorBlock*
-                          >(1,*hadamard_s);
-  auto *hadamard_e = new Hadamard_FilteredBitVectorBlock(var_e,1,*join_s);
-  auto *join_e = new Join < FilteredBitVectorBlock*,
-                            FilteredBitVectorBlock*
-                          >(1,*hadamard_e);
-  auto *hadamard_d = new Hadamard_FilteredBitVectorBlock(var_e,1,*join_e);
-  auto *join_d = new Join < FilteredBitVectorBlock*,
-                            FilteredBitVectorBlock*
-                          >(1,*hadamard_d);
-  auto *dot = new Dot_BitmapBlock(var_a_pred,var_a,1,*join_d);
+  auto *sum = new Sum(var_h,2);
+  auto *hadamard_s = new Hadamard_DecimalVectorBlock_2(var_g,1,*sum, "HAD_S");
+  auto *hadamard_e = new Hadamard_FilteredBitVectorBlock_2(var_e,1,*hadamard_s, "HAD_E");
+  auto *hadamard_d = new Hadamard_FilteredBitVectorBlock_2(var_e,1,*hadamard_e, "HAD_D");
+  auto *dot = new Dot_BitmapBlock(var_a_pred,var_a,2,*hadamard_d);
   auto *load_a2 = new Load_BitmapBlock(var_lineitem_shipdate,*dot);
   auto *filter_b = new Filter_DecimalVectorBlock(
-                        var_b,*filter_var_b,1,
-                        join_d->in_join,join_d->done_join
+                        var_b,*filter_var_b,3,
+                        hadamard_d->in_join,hadamard_d->done_join
                         );
   auto *load_be = new Load_DecimalVectorBlock(var_lineitem_discount,*filter_b);
   auto *filter_c = new Filter_DecimalVectorBlock(
-                        var_d,*filter_var_d,1,
-                        join_e->in_join,join_e->done_join
-                        );
+                        var_d,*filter_var_d,2,
+                        hadamard_e->in_join,hadamard_e->done_join
+                        ); //Multi-thread traz problemas ...
   auto *load_c= new Load_DecimalVectorBlock(var_lineitem_quantity,*filter_c);
-  Map *map = new Map(var_f,1,join_s->in_join,join_s->done_join);
-  auto *join_m = new Join<DecimalVectorBlock*,DecimalVectorBlock*>(1,*map);
-  load_be->add_out(join_m->in_join,join_m->done_join);
-  auto *load_f = new Load_DecimalVectorBlock(var_lineitem_extendedprice,*join_m);
+  auto *map = new Map_2(var_f,2,hadamard_s->in_join,hadamard_s->done_join, "MAP"); //Multi-thread traz problemas ...
+  load_be->add_out(map->in_join,map->done_join);
+  auto *load_f = new Load_DecimalVectorBlock(var_lineitem_extendedprice,*map);
 
   boost::thread *loader_a1_t,*filter_a_t,*loader_a2_t,*dot_a_t,
                 *loader_be_t,*filter_b_t,*had_d_t,*loader_c_t,*filter_c_t,
-                *had_e_t,*loader_f_t,*map_t,*had_s_t,*sum_t,*join_d_t,*join_e_t,
-                *join_s_t,*join_m_t;
+                *had_e_t,*loader_f_t,*map_t,*had_s_t,*sum_t;
 
 
   loader_a1_t = new boost::thread(&Load_LabelBlock::run_seq, loader_a1);
-  filter_a_t = new boost::thread(&Filter_LabelBlock::run_seq, filter_a);
+  filter_a_t = new boost::thread(&Filter_LabelBlock::run, filter_a);
   loader_be_t = new boost::thread(&Load_DecimalVectorBlock::run_seq, load_be);  
-  filter_b_t = new boost::thread(&Filter_DecimalVectorBlock::run_seq, filter_b);
+  filter_b_t = new boost::thread(&Filter_DecimalVectorBlock::run, filter_b);
 
   loader_a1_t->join();
   loader_a2_t = new boost::thread(&Load_BitmapBlock::run_seq, load_a2);
   filter_a_t->join();
 
-  dot_a_t = new boost::thread(&Dot_BitmapBlock::run_seq, dot);
-  join_d_t = new boost::thread(&Join< FilteredBitVectorBlock*,
-                                      FilteredBitVectorBlock*
-                                    >::run_seq, join_d);
-  had_d_t = new boost::thread(&Hadamard_FilteredBitVectorBlock::run_seq, hadamard_d);
+  dot_a_t = new boost::thread(&Dot_BitmapBlock::run, dot);
+  had_d_t = new boost::thread(&Hadamard_FilteredBitVectorBlock_2::run_seq, hadamard_d);
   loader_be_t->join();
   loader_c_t = new boost::thread(&Load_DecimalVectorBlock::run_seq, load_c);
+  filter_c_t = new boost::thread(&Filter_DecimalVectorBlock::run, filter_c);
   loader_a2_t->join();
-  filter_c_t = new boost::thread(&Filter_DecimalVectorBlock::run_seq, filter_c);
   dot_a_t->join();
-  join_e_t = new boost::thread(&Join< FilteredBitVectorBlock*,
-                                      FilteredBitVectorBlock*
-                                    >::run_seq, join_e);
-  had_e_t = new boost::thread(&Hadamard_FilteredBitVectorBlock::run_seq, hadamard_e);
+  had_e_t = new boost::thread(&Hadamard_FilteredBitVectorBlock_2::run_seq, hadamard_e);
   loader_f_t = new boost::thread(&Load_DecimalVectorBlock::run_seq, load_f);
   filter_b_t->join();
-  join_m_t = new boost::thread(&Join< DecimalVectorBlock*,
-                                      DecimalVectorBlock*
-                                    >::run_seq, join_m);
-  map_t = new boost::thread(&Map::run_seq, map);
-  join_d_t->join();
-  join_s_t = new boost::thread(&Join< FilteredBitVectorBlock*,
-                                      DecimalVectorBlock*
-                                    >::run_seq, join_s);
-  had_s_t = new boost::thread(&Hadamard_DecimalVectorBlock::run_seq, hadamard_s);
-  sum_t = new boost::thread(&Sum::run_seq, sum);
+  map_t = new boost::thread(&Map_2::run_seq, map); //unsorted_Map não é thread safe ...
+  had_s_t = new boost::thread(&Hadamard_DecimalVectorBlock_2::run_seq, hadamard_s);
+  sum_t = new boost::thread(&Sum::run_seq, sum); //Multi-thread traz problemas ...
 
   had_d_t->join();
   loader_c_t->join();
   filter_c_t->join();
-  join_e_t->join();
   had_e_t->join();
   loader_f_t->join();
-  join_m_t->join();
   map_t->join();
-  join_s_t->join();
   had_s_t->join();
   sum_t->join();
 
@@ -145,9 +118,11 @@ int main() {
 
   std::cout << (*var_h) << std::endl;
 
-  delete var_h;
+  delete var_a;delete var_b;delete var_c;delete var_d;
+  delete var_e;delete var_f;delete var_g;delete var_h;
 
   auto end = std::chrono::high_resolution_clock::now();
+  
   std::cout
 	 << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()
    << endl
